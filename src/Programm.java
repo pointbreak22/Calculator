0ea2a9a2cf87ccc7865735c015429c6a3e7a5416
Влад Зброжек <pointbreak22@outlook.com>
2020-10-15 23:19:31 +0300

    Initial commit

---
+++ src/Programm.java
@@ -0,0 +1,13 @@
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class Programm {
+
+
+    public static void main(String[] args)
+    { Scanner in = new Scanner(System.in);
+        out.println("Введите строку \n");
+      String text= in.nextLine();
+        out.println(text);
+    }
+}
